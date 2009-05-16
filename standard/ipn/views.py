6262255bfbadbee968f43a240eb04e5decc6690e
John Boxall <john@handimobility.ca>
2009-05-16 14:46:06 -0700

    + Fix missing object bug - thanks Michael.

--- standard/ipn/views.py
+++ standard/ipn/views.py
@@ -17,23 +17,26 @@ def ipn(request, item_check_callable=None):
     https://developer.paypal.com/cgi-bin/devscr?cmd=_ipn-link-session
     
     """    
+    flag = None
     form = PayPalIPNForm(request.POST)
     if form.is_valid():
         try:
             ipn_obj = form.save(commit=False)
         except Exception, e:
-            ipn_obj = PayPalIPN()
-            ipn_obj.set_flag("Exception while processing. (%s)" % form.errors)
+            flag = "Exception while processing. (%s)" % e
     else:
-        ipn_obj.set_flag("Invalid form. (%s)" % form.errors)
+        flag = "Invalid form. (%s)" % form.errors
 
-    ipn_obj.initialize(request)
-    if not ipn_obj.flag:
+    if flag is not None:
+        ipn_obj = PayPalIPN()
+        ipn_obj.set_flag(flag)
+    else:
         # Secrets should only be used over SSL.
         if request.is_secure() and 'secret' in request.GET:
             ipn_obj.verify_secret(form, request.GET['secret'])
         else:
             ipn_obj.verify(item_check_callable)
 
+    ipn_obj.initialize(request)
     ipn_obj.save()
     return HttpResponse("OKAY")
