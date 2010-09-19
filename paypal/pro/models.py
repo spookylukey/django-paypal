3066b39eb821b38e5cf7bf48063bfd9a6ffb9015
Chris Chambers <magma.chambers@gmail.com>
2010-09-19 18:56:11 +0100

    Stripping port-info from PayPalNVP ipaddress field
    This is necessary to avoid errors from postgres backends.

--- paypal/pro/models.py
+++ paypal/pro/models.py
@@ -59,7 +59,7 @@ class PayPalNVP(Model):
     
     def init(self, request, paypal_request, paypal_response):
         """Initialize a PayPalNVP instance from a HttpRequest."""
-        self.ipaddress = request.META.get('REMOTE_ADDR', '')
+        self.ipaddress = request.META.get('REMOTE_ADDR', '').split(':')[0]
         if hasattr(request, "user") and request.user.is_authenticated():
             self.user = request.user
 
