9118b9d396a70666ceee60f3b56a486978fd830d
Luke Plant <L.Plant.98@cantab.net>
2014-01-03 11:02:33 +0000

    Added Python 2.7 classifier

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(
         "Intended Audience :: Developers",
         "Intended Audience :: System Administrators",
         "Operating System :: OS Independent",
-        "Topic :: Software Development"
+        "Topic :: Software Development",
+        "Programming Language :: Python :: 2.7",
     ],
 )
