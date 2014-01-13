spookylukey/django-paypal

commit 451af2e6b51d1b678de503b0f72f512259b00185

Python 3 compatibility

paypal/standard/pdt/admin.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from string import split as L
 from django.contrib import admin
 from paypal.standard.pdt.models import PayPalPDT
 
@@ -10,45 +9,113 @@ class PayPalPDTAdmin(admin.ModelAdmin):
     date_hierarchy = 'payment_date'
     fieldsets = (
         (None, {
-            "fields": L(
-                "flag txn_id txn_type payment_status payment_date transaction_entity reason_code pending_reason mc_gross mc_fee auth_status auth_amount auth_exp auth_id")
+            "fields":
+                ['flag',
+                 'txn_id',
+                 'txn_type',
+                 'payment_status',
+                 'payment_date',
+                 'transaction_entity',
+                 'reason_code',
+                 'pending_reason',
+                 'mc_gross',
+                 'mc_fee',
+                 'auth_status',
+                 'auth_amount',
+                 'auth_exp',
+                 'auth_id',
+                 ],
         }),
         ("Address", {
             "description": "The address of the Buyer.",
             'classes': ('collapse',),
-            "fields": L(
-                "address_city address_country address_country_code address_name address_state address_status address_street address_zip")
+            "fields":
+                ['address_city',
+                 'address_country',
+                 'address_country_code',
+                 'address_name',
+                 'address_state',
+                 'address_status',
+                 'address_street',
+                 'address_zip',
+                 ],
         }),
         ("Buyer", {
             "description": "The information about the Buyer.",
             'classes': ('collapse',),
-            "fields": L(
-                "first_name last_name payer_business_name payer_email payer_id payer_status contact_phone residence_country")
+            "fields":
+                ['first_name',
+                 'last_name',
+                 'payer_business_name',
+                 'payer_email',
+                 'payer_id',
+                 'payer_status',
+                 'contact_phone',
+                 'residence_country'
+                 ],
         }),
         ("Seller", {
             "description": "The information about the Seller.",
             'classes': ('collapse',),
-            "fields": L("business item_name item_number quantity receiver_email receiver_id custom invoice memo")
+            "fields":
+                ['business',
+                 'item_name',
+                 'item_number',
+                 'quantity',
+                 'receiver_email',
+                 'receiver_id',
+                 'custom',
+                 'invoice',
+                 'memo',
+                 ],
         }),
         ("Subscriber", {
             "description": "The information about the Subscription.",
             'classes': ('collapse',),
-            "fields": L("subscr_id subscr_date subscr_effective")
+            "fields":
+                ['subscr_id',
+                 'subscr_date',
+                 'subscr_effective',
+                 ],
         }),
         ("Recurring", {
             "description": "Information about recurring Payments.",
             "classes": ("collapse",),
-            "fields": L(
-                "profile_status initial_payment_amount  amount_per_cycle outstanding_balance period_type product_name product_type recurring_payment_id receipt_id next_payment_date")
+            "fields":
+                ['profile_status',
+                 'initial_payment_amount',
+                 'amount_per_cycle',
+                 'outstanding_balance',
+                 'period_type',
+                 'product_name',
+                 'product_type',
+                 'recurring_payment_id',
+                 'receipt_id',
+                 'next_payment_date',
+                 ],
         }),
         ("Admin", {
             "description": "Additional Info.",
             "classes": ('collapse',),
-            "fields": L("test_ipn ipaddress query flag_code flag_info")
+            "fields":
+                ['test_ipn',
+                 'ipaddress',
+                 'query',
+                 'flag_code',
+                 'flag_info',
+                 ],
         }),
     )
-    list_display = L("__unicode__ flag invoice custom payment_status created_at")
-    search_fields = L("txn_id recurring_payment_id")
+    list_display = ["__unicode__",
+                    "flag".
+                    "invoice",
+                    "custom",
+                    "payment_status",
+                    "created_at",
+                    ]
+    search_fields = ["txn_id",
+                     "recurring_payment_id".
+                     ]
 
 
 admin.site.register(PayPalPDT, PayPalPDTAdmin)

paypal/pro/creditcard.py
@@ -59,7 +59,7 @@ class CreditCard(object):
 
     def get_type(self):
         """Return the type if it matches one of the cards."""
-        for card, pattern in CARDS.iteritems():
+        for card, pattern in CARDS.items():
             if pattern.match(self.number):
                 return card
         return None

paypal/pro/fields.py
@@ -5,6 +5,7 @@ from datetime import date
 
 from django import forms
 from django.utils.translation import ugettext as _
+from six.moves import range
 
 from paypal.pro.creditcard import verify_credit_card
 
@@ -45,8 +46,8 @@ class CreditCardExpiryWidget(forms.MultiWidget):
 
 
 class CreditCardExpiryField(forms.MultiValueField):
-    EXP_MONTH = [(x, x) for x in xrange(1, 13)]
-    EXP_YEAR = [(x, x) for x in xrange(date.today().year, date.today().year + 15)]
+    EXP_MONTH = [(x, x) for x in range(1, 13)]
+    EXP_YEAR = [(x, x) for x in range(date.today().year, date.today().year + 15)]
 
     default_error_messages = {
         'invalid_month': u'Enter a valid month.',

paypal/pro/helpers.py
@@ -5,16 +5,17 @@ import datetime
 import logging
 import pprint
 import time
-import urllib
-import urllib2
 
 from django.conf import settings
 from django.forms.models import fields_for_model
+from django.http import QueryDict
 from django.utils.datastructures import MergeDict
 from django.utils.http import urlencode
+from six.moves.urllib.parse import unquote
+from six.moves.urllib.request import urlopen
 
 from paypal.pro.signals import *
-from paypal.pro.models import PayPalNVP, L
+from paypal.pro.models import PayPalNVP
 from paypal.pro.exceptions import PayPalFailure
 
 TEST = settings.PAYPAL_TEST
@@ -25,7 +26,7 @@ VERSION = 54.0
 BASE_PARAMS = dict(USER=USER, PWD=PASSWORD, SIGNATURE=SIGNATURE, VERSION=VERSION)
 ENDPOINT = "https://api-3t.paypal.com/nvp"
 SANDBOX_ENDPOINT = "https://api-3t.sandbox.paypal.com/nvp"
-NVP_FIELDS = fields_for_model(PayPalNVP).keys()
+NVP_FIELDS = list(fields_for_model(PayPalNVP).keys())
 
 
 log = logging.getLogger(__file__)
@@ -71,8 +72,20 @@ class PayPalWPP(object):
     def doDirectPayment(self, params):
         """Call PayPal DoDirectPayment method."""
         defaults = {"method": "DoDirectPayment", "paymentaction": "Sale"}
-        required = L(
-            "creditcardtype acct expdate cvv2 ipaddress firstname lastname street city state countrycode zip amt")
+        required = ["creditcardtype",
+                    "acct",
+                    "expdate",
+                    "cvv2",
+                    "ipaddress",
+                    "firstname",
+                    "lastname",
+                    "street",
+                    "city",
+                    "state",
+                    "countrycode",
+                    "zip",
+                    "amt",
+                    ]
         nvp_obj = self._fetch(params, required, defaults)
         if nvp_obj.flag:
             raise PayPalFailure(nvp_obj.flag_info)
@@ -96,7 +109,7 @@ class PayPalWPP(object):
             params = self._recurring_setExpressCheckout_adapter(params)
 
         defaults = {"method": "SetExpressCheckout", "noshipping": 1}
-        required = L("returnurl cancelurl amt")
+        required = ["returnurl", "cancelurl", "amt"]
         nvp_obj = self._fetch(params, required, defaults)
         if nvp_obj.flag:
             raise PayPalFailure(nvp_obj.flag_info)
@@ -107,7 +120,7 @@ class PayPalWPP(object):
         Check the dude out:
         """
         defaults = {"method": "DoExpressCheckoutPayment", "paymentaction": "Sale"}
-        required = L("returnurl cancelurl amt token payerid")
+        required = ["returnurl", "cancelurl", "amt", "token payerid"]
         nvp_obj = self._fetch(params, required, defaults)
         if nvp_obj.flag:
             raise PayPalFailure(nvp_obj.flag_info)
@@ -120,13 +133,13 @@ class PayPalWPP(object):
         Returns True PayPal successfully creates the profile otherwise False.
         """
         defaults = {"method": "CreateRecurringPaymentsProfile"}
-        required = L("profilestartdate billingperiod billingfrequency amt")
+        required = ["profilestartdate", "billingperiod", "billingfrequency", "amt"]
 
         # Direct payments require CC data
         if direct:
-            required + L("creditcardtype acct expdate firstname lastname")
+            required + ["creditcardtype", "acct", "expdate", "firstname", "lastname"]
         else:
-            required + L("token payerid")
+            required + ["token", "payerid"]
 
         nvp_obj = self._fetch(params, required, defaults)
 
@@ -138,7 +151,7 @@ class PayPalWPP(object):
 
     def getExpressCheckoutDetails(self, params):
         defaults = {"method": "GetExpressCheckoutDetails"}
-        required = L("token")
+        required = ["token"]
         nvp_obj = self._fetch(params, required, defaults)
         if nvp_obj.flag:
             raise PayPalFailure(nvp_obj.flag_info)
@@ -149,7 +162,7 @@ class PayPalWPP(object):
 
     def getTransactionDetails(self, params):
         defaults = {"method": "GetTransactionDetails"}
-        required = L("transactionid")
+        required = ["transactionid"]
 
         nvp_obj = self._fetch(params, required, defaults)
         if nvp_obj.flag:
@@ -164,7 +177,7 @@ class PayPalWPP(object):
 
     def updateRecurringPaymentsProfile(self, params):
         defaults = {"method": "UpdateRecurringPaymentsProfile"}
-        required = L("profileid")
+        required = ["profileid"]
 
         nvp_obj = self._fetch(params, required, defaults)
         if nvp_obj.flag:
@@ -180,7 +193,7 @@ class PayPalWPP(object):
         Action must be either "Cancel", "Suspend", or "Reactivate".
         """
         defaults = {"method": "ManageRecurringPaymentsProfileStatus"}
-        required = L("profileid action")
+        required = ["profileid", "action"]
 
         nvp_obj = self._fetch(params, required, defaults)
 
@@ -212,7 +225,7 @@ class PayPalWPP(object):
         params['l_billingtype0'] = "RecurringPayments"
         params['l_billingagreementdescription0'] = params['desc']
 
-        REMOVE = L("billingfrequency billingperiod profilestartdate desc")
+        REMOVE = ["billingfrequency", "billingperiod", "profilestartdate", "desc"]
         for k in params.keys():
             if k in REMOVE:
                 del params[k]
@@ -248,7 +261,7 @@ class PayPalWPP(object):
 
     def _request(self, data):
         """Moved out to make testing easier."""
-        return urllib2.urlopen(self.endpoint, data).read()
+        return urlopen(self.endpoint, data.encode("ascii")).read()
 
     def _check_and_update_params(self, required, params):
         """
@@ -260,12 +273,9 @@ class PayPalWPP(object):
                 raise PayPalError("Missing required param: %s" % r)
 
         # Upper case all the parameters for PayPal.
-        return (dict((k.upper(), v) for k, v in params.iteritems()))
+        return (dict((k.upper(), v) for k, v in params.items()))
 
     def _parse_response(self, response):
         """Turn the PayPal response into a dict"""
-        response_tokens = {}
-        for kv in response.split('&'):
-            key, value = kv.split("=")
-            response_tokens[key.lower()] = urllib.unquote(value)
-        return response_tokens
+        q = QueryDict(response, encoding='UTF-8').dict()
+        return {k.lower(): v for k,v in q.items()}

paypal/pro/models.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from string import split as L
 from django.db import models
 from django.utils.http import urlencode
 from django.forms.models import model_to_dict
@@ -15,10 +14,32 @@ except ImportError:
 class PayPalNVP(Model):
     """Record of a NVP interaction with PayPal."""
     TIMESTAMP_FORMAT = "%Y-%m-%dT%H:%M:%SZ"  # 2009-02-03T17:47:41Z
-    RESTRICTED_FIELDS = L("expdate cvv2 acct")
-    ADMIN_FIELDS = L("id user flag flag_code flag_info query response created_at updated_at ")
-    ITEM_FIELDS = L("amt custom invnum")
-    DIRECT_FIELDS = L("firstname lastname street city state countrycode zip")
+    RESTRICTED_FIELDS = ["expdate",
+                         "cvv2",
+                         "acct",
+                         ]
+    ADMIN_FIELDS = ['id',
+                    'user',
+                    'flag',
+                    'flag_code',
+                    'flag_info',
+                    'query',
+                    'response',
+                    'created_at',
+                    'updated_at',
+                    ]
+    ITEM_FIELDS = ["amt",
+                   "custom",
+                   "invnum",
+                   ]
+    DIRECT_FIELDS = ["firstname",
+                     "lastname",
+                     "street",
+                     "city",
+                     "state",
+                     "countrycode",
+                     "zip",
+                     ]
 
     # Response fields
     method = models.CharField(max_length=64, blank=True)
@@ -66,7 +87,7 @@ class PayPalNVP(Model):
             self.user = request.user
 
         # No storing credit card info.
-        query_data = dict((k, v) for k, v in paypal_request.iteritems() if k not in self.RESTRICTED_FIELDS)
+        query_data = dict((k, v) for k, v in paypal_request.items() if k not in self.RESTRICTED_FIELDS)
         self.query = urlencode(query_data)
         self.response = urlencode(paypal_response)
 

paypal/pro/tests.py
@@ -99,7 +99,7 @@ class PayPalWPPTest(TestCase):
         # We'll have to stub out tests for doExpressCheckoutPayment and friends
         # because they're behind paypal's doors.
         nvp_obj = self.wpp.setExpressCheckout(self.item)
-        self.assertTrue(nvp_obj.ack == "Success")
+        self.assertEqual(nvp_obj.ack, "Success")
 
 
 ### DoExpressCheckoutPayment

paypal/standard/forms.py
@@ -153,7 +153,7 @@ class PayPalEncryptedPaymentsForm(PayPalPaymentsForm):
 
         # Iterate through the fields and pull out the ones that have a value.
         plaintext = 'cert_id=%s\n' % CERT_ID
-        for name, field in self.fields.iteritems():
+        for name, field in self.fields.items():
             value = None
             if name in self.initial:
                 value = self.initial[name]

paypal/standard/ipn/models.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import urllib2
+from six.moves.urllib.request import urlopen
+
 from paypal.standard.models import PayPalStandardBase
 from paypal.standard.ipn.signals import *
 
@@ -15,7 +16,7 @@ class PayPalIPN(PayPalStandardBase):
 
     def _postback(self):
         """Perform PayPal Postback validation."""
-        return urllib2.urlopen(self.get_endpoint(), "cmd=_notify-validate&%s" % self.query).read()
+        return urlopen(self.get_endpoint(), "cmd=_notify-validate&%s" % self.query).read()
 
     def _verify_postback(self):
         if self.response != "VERIFIED":

commit 6fe640806a9a3a9b2b44f31398bb5af71aaac7d2

Fixed some syntax errors introduced in 451af2e
Thanks prplz for the catch.

paypal/standard/pdt/admin.py
@@ -107,14 +107,14 @@ class PayPalPDTAdmin(admin.ModelAdmin):
         }),
     )
     list_display = ["__unicode__",
-                    "flag".
+                    "flag",
                     "invoice",
                     "custom",
                     "payment_status",
                     "created_at",
                     ]
     search_fields = ["txn_id",
-                     "recurring_payment_id".
+                     "recurring_payment_id",
                      ]
 
 