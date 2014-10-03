334bb1b5b3ea2dd1bb3cdcdf83c6c496fd9cfd67
Luke Plant <L.Plant.98@cantab.net>
2014-10-03 14:55:07 +0100

    Display warnings when running tests

--- runtests.py
+++ runtests.py
@@ -4,6 +4,10 @@ import sys
 import django
 from django.conf import settings
 
+import warnings
+warnings.simplefilter("always", PendingDeprecationWarning)
+warnings.simplefilter("always", DeprecationWarning)
+
 settings.configure(
     ROOT_URLCONF='',
     DATABASES={'default': {'ENGINE': 'django.db.backends.sqlite3'}},
