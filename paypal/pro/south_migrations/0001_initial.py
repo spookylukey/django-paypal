6e9571f53b0bb7db4a3960cceeaee1d4c96c4489
Luke Plant <L.Plant.98@cantab.net>
2014-11-17 13:03:26 +0000

    Added initial South migrations for pro app

---
+++ paypal/pro/south_migrations/0001_initial.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'PayPalNVP'
+        db.create_table('paypal_nvp', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('method', self.gf('django.db.models.fields.CharField')(max_length=64, blank=True)),
+            ('ack', self.gf('django.db.models.fields.CharField')(max_length=32, blank=True)),
+            ('profilestatus', self.gf('django.db.models.fields.CharField')(max_length=32, blank=True)),
+            ('timestamp', self.gf('django.db.models.fields.DateTimeField')(null=True, blank=True)),
+            ('profileid', self.gf('django.db.models.fields.CharField')(max_length=32, blank=True)),
+            ('profilereference', self.gf('django.db.models.fields.CharField')(max_length=128, blank=True)),
+            ('correlationid', self.gf('django.db.models.fields.CharField')(max_length=32, blank=True)),
+            ('token', self.gf('django.db.models.fields.CharField')(max_length=64, blank=True)),
+            ('payerid', self.gf('django.db.models.fields.CharField')(max_length=64, blank=True)),
+            ('firstname', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
+            ('lastname', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
+            ('street', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
+            ('city', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
+            ('state', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
+            ('countrycode', self.gf('django.db.models.fields.CharField')(max_length=2, blank=True)),
+            ('zip', self.gf('django.db.models.fields.CharField')(max_length=32, blank=True)),
+            ('invnum', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
+            ('custom', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
+            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'], null=True, blank=True)),
+            ('flag', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('flag_code', self.gf('django.db.models.fields.CharField')(max_length=32, blank=True)),
+            ('flag_info', self.gf('django.db.models.fields.TextField')(blank=True)),
+            ('ipaddress', self.gf('django.db.models.fields.IPAddressField')(max_length=15, blank=True)),
+            ('query', self.gf('django.db.models.fields.TextField')(blank=True)),
+            ('response', self.gf('django.db.models.fields.TextField')(blank=True)),
+            ('created_at', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('updated_at', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
+        ))
+        db.send_create_signal(u'pro', ['PayPalNVP'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'PayPalNVP'
+        db.delete_table('paypal_nvp')
+
+
+    models = {
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Group']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'pro.paypalnvp': {
+            'Meta': {'object_name': 'PayPalNVP', 'db_table': "'paypal_nvp'"},
+            'ack': ('django.db.models.fields.CharField', [], {'max_length': '32', 'blank': 'True'}),
+            'city': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'correlationid': ('django.db.models.fields.CharField', [], {'max_length': '32', 'blank': 'True'}),
+            'countrycode': ('django.db.models.fields.CharField', [], {'max_length': '2', 'blank': 'True'}),
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'custom': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'firstname': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'flag': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'flag_code': ('django.db.models.fields.CharField', [], {'max_length': '32', 'blank': 'True'}),
+            'flag_info': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'invnum': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'ipaddress': ('django.db.models.fields.IPAddressField', [], {'max_length': '15', 'blank': 'True'}),
+            'lastname': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'method': ('django.db.models.fields.CharField', [], {'max_length': '64', 'blank': 'True'}),
+            'payerid': ('django.db.models.fields.CharField', [], {'max_length': '64', 'blank': 'True'}),
+            'profileid': ('django.db.models.fields.CharField', [], {'max_length': '32', 'blank': 'True'}),
+            'profilereference': ('django.db.models.fields.CharField', [], {'max_length': '128', 'blank': 'True'}),
+            'profilestatus': ('django.db.models.fields.CharField', [], {'max_length': '32', 'blank': 'True'}),
+            'query': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'response': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'state': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'street': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'timestamp': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'token': ('django.db.models.fields.CharField', [], {'max_length': '64', 'blank': 'True'}),
+            'updated_at': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']", 'null': 'True', 'blank': 'True'}),
+            'zip': ('django.db.models.fields.CharField', [], {'max_length': '32', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['pro']
