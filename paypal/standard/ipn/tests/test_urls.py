6483deef00af75127a254f5674c2b2e3b14138ff
Luke Plant <L.Plant.98@cantab.net>
2016-03-30 12:43:29 +0100

    Fixed test suite under Django 1.6

--- paypal/standard/ipn/tests/test_urls.py
+++ paypal/standard/ipn/tests/test_urls.py
@@ -1,11 +1,22 @@
 from __future__ import unicode_literals
 
-from django.conf.urls import url
+import django
+from django.conf.urls import url, include
+from django.contrib import admin
 
 from paypal.standard.ipn import views
-from django.contrib import admin
+
+if django.VERSION < (1, 7):
+    import paypal.standard.ipn.admin  # noqa
+    admin.autodiscover()
+
 
 urlpatterns = [
     url(r'^ipn/$', views.ipn),
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', include(admin.site.urls)),
 ]
+
+
+if django.VERSION < (1, 8):
+    from django.conf.urls import patterns
+    urlpatterns = patterns('', *urlpatterns)
