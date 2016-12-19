7db7bea0e831754ee20e4a7dab888a0819b29f6a
Nicolas SURBAYROLE <nicolas.surbayrole@inpt.fr>
2016-12-19 17:49:17 +0100

    Move certificate in conf.py

--- paypal/standard/forms.py
+++ paypal/standard/forms.py
@@ -14,7 +14,8 @@ from django.utils.translation import ugettext_lazy as _
 
 from paypal.standard.conf import (
     DONATION_IMAGE, DONATION_SANDBOX_IMAGE, IMAGE, POSTBACK_ENDPOINT, SANDBOX_IMAGE, SANDBOX_POSTBACK_ENDPOINT,
-    SUBSCRIPTION_IMAGE, SUBSCRIPTION_SANDBOX_IMAGE
+    SUBSCRIPTION_IMAGE, SUBSCRIPTION_SANDBOX_IMAGE, PAYPAL_PRIVATE_CERT, PAYPAL_PUBLIC_CERT, PAYPAL_CERT,
+    PAYPAL_CERT_ID
 )
 from paypal.standard.widgets import ReservedValueHiddenInput, ValueHiddenInput
 
@@ -229,14 +230,9 @@ class PayPalEncryptedPaymentsForm(PayPalPaymentsForm):
     def _encrypt(self):
         """Use your key thing to encrypt things."""
         from M2Crypto import BIO, SMIME, X509
-        # @@@ Could we move this to conf.py?
-        CERT = settings.PAYPAL_PRIVATE_CERT
-        PUB_CERT = settings.PAYPAL_PUBLIC_CERT
-        PAYPAL_CERT = settings.PAYPAL_CERT
-        CERT_ID = settings.PAYPAL_CERT_ID
 
         # Iterate through the fields and pull out the ones that have a value.
-        plaintext = 'cert_id=%s\n' % CERT_ID
+        plaintext = 'cert_id=%s\n' % PAYPAL_CERT_ID
         for name, field in self.fields.items():
             value = None
             if name in self.initial:
@@ -252,7 +248,7 @@ class PayPalEncryptedPaymentsForm(PayPalPaymentsForm):
 
         # Begin crypto weirdness.
         s = SMIME.SMIME()
-        s.load_key_bio(BIO.openfile(CERT), BIO.openfile(PUB_CERT))
+        s.load_key_bio(BIO.openfile(PAYPAL_PRIVATE_CERT), BIO.openfile(PAYPAL_PUBLIC_CERT))
         p7 = s.sign(BIO.MemoryBuffer(plaintext), flags=SMIME.PKCS7_BINARY)
         x509 = X509.load_cert_bio(BIO.openfile(PAYPAL_CERT))
         sk = X509.X509_Stack()
