60e8a4281d20b59e5235798326e1224ed79ad5d3
Luke Plant <L.Plant.98@cantab.net>
2017-02-01 13:11:50 +0300

    Fixed error in PayPalNVPAdmin

--- paypal/pro/admin.py
+++ paypal/pro/admin.py
@@ -10,7 +10,7 @@ from paypal.pro.models import PayPalNVP
 class PayPalNVPAdmin(admin.ModelAdmin):
     list_display = ('user', 'ipaddress', 'method', 'flag', 'flag_code', 'created_at')
     list_filter = ('flag', 'created_at')
-    search_fields = ('user__email', 'ip_address', 'flag', 'firstname', 'lastname')
+    search_fields = ('user__email', 'ipaddress', 'flag', 'firstname', 'lastname')
 
 
 admin.site.register(PayPalNVP, PayPalNVPAdmin)
