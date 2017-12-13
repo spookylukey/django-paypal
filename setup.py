9748254f80016ca262a5a39597e224708e9084ce
Jonathan Roach (FAP) <frameworkaccesspoint@icloud.com>
2017-12-13 12:01:47 +0000

    PayPalPaymentsForm: remove redundent, explicit field definitions; deprecate initial['return_url'], use initia['return'] instead; add new cmd choices.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ DOCS_URL = 'https://django-paypal.readthedocs.org'
 
 setup(
     name='django-paypal',
-    version="0.4.1",
+    version="0.5.0",
     author='John Boxall',
     author_email='john@handimobility.ca',
     maintainer="Luke Plant",
