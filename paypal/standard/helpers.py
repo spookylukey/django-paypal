2eda6c4eae503ecf634739dd0bb68e7f3334b1ac
Emilio Moretti <emilio.moretti@gmail.com>
2020-07-06 07:19:13 -0300

    Fix PayPalSharedSecretEncryptedPaymentsForm in Python 3
    Clean up PRs #223 #217 #211
    Addressed comments and cleaned up flake8 errors.

--- paypal/standard/helpers.py
+++ paypal/standard/helpers.py
@@ -7,10 +7,12 @@ from django.utils.encoding import smart_str
 
 from paypal.utils import warn_untested
 
+import six
+
 
 def get_sha1_hexdigest(salt, raw_password):
-    warn_untested()
-    return hashlib.sha1(smart_str(salt) + smart_str(raw_password)).hexdigest()
+    encoded_string = (smart_str(salt) + smart_str(raw_password)).encode('utf-8')
+    return hashlib.sha1(encoded_string).hexdigest()
 
 
 def duplicate_txn_id(ipn_obj):
@@ -57,13 +59,13 @@ def make_secret(form_instance, secret_fields=None):
     for name in secret_fields:
         if hasattr(form_instance, 'cleaned_data'):
             if name in form_instance.cleaned_data:
-                data += unicode(form_instance.cleaned_data[name])
+                data += six.text_type(form_instance.cleaned_data[name])
         else:
             # Initial data passed into the constructor overrides defaults.
             if name in form_instance.initial:
-                data += unicode(form_instance.initial[name])
+                data += six.text_type(form_instance.initial[name])
             elif name in form_instance.fields and form_instance.fields[name].initial is not None:
-                data += unicode(form_instance.fields[name].initial)
+                data += six.text_type(form_instance.fields[name].initial)
 
     secret = get_sha1_hexdigest(settings.SECRET_KEY, data)
     return secret
